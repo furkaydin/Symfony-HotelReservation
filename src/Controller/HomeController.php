eeb52a4fd2b8fad10f9e611635de640d735c1253
furkaydin <furkay16@gmail.com>
2022-01-29 19:07:09 +0300

    home page slider

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Hotel;
+use App\Repository\HotelRepository;
 use App\Repository\SettingRepository;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +15,24 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(SettingRepository $settingRepository): Response
+    public function index(SettingRepository $settingRepository, HotelRepository $hotelRepository): Response
     {
         $setting=$settingRepository->findAll();
+        $slider=$hotelRepository->findBy(array(),array(),3);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'setting'=>$setting,
+            'slider'=>$slider,
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="hotel_show", methods={"GET"})
+     */
+    public function show(Hotel $hotel): Response
+    {
+        return $this->render('home/hotelshow.html.twig', [
+            'hotel' => $hotel,
         ]);
     }
 }
